92b48f63ebd300143463208a34cf1053af3af1dc
Hariton Bogdan <hariton.bogdan11@gmail.com>
2023-03-19 22:16:32 +0200

    YC Basic Authorization FP

--- seeds/index.js
+++ seeds/index.js
@@ -18,6 +18,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6414ca1d203fe518736aa863',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)} `,
             image: 'https://source.unsplash.com/collection/483251',
