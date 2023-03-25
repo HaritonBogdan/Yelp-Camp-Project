141f249cbacbdadb76dccab6051dd1f1c4c67ff0
Hariton Bogdan <hariton.bogdan11@gmail.com>
2023-03-25 22:10:35 +0200

    YC Image Upload SP

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,11 +11,8 @@ const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.array('image'), (req, res) => {
-        console.log(req.body, req.files);
-        res.redirect('/campgrounds')
-    })
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
