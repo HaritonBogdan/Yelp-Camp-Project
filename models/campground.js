157088d7802982b8f6ae5e1a8ad09940ef8feb58
Hariton Bogdan <hariton.bogdan11@gmail.com>
2023-03-28 20:06:50 +0300

    YC Styles Clean Up

--- models/campground.js
+++ models/campground.js
@@ -5,11 +5,13 @@ const Schema = mongoose.Schema;
 const ImageSchema = new Schema({
     url: String,
     filename: String
-})
+});
 
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
-})
+});
+
+const opts = { toJSON: { virtuals: true } };
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -38,7 +40,12 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
-})
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>`
+});
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
@@ -48,6 +55,6 @@ CampgroundSchema.post('findOneAndDelete', async function (doc) {
             }
         })
     }
-})
+});
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
